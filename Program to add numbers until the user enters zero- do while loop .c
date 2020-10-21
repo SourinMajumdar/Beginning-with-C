74b27ebd2b6f7087233d886bf10d72500069e2c7
Sourin Majumdar <71887222+SourinMajumdar@users.noreply.github.com>
2020-10-21 21:21:47 +0530

    Create Program to add numbers until the user enters zero- do while loop .c

---
+++ Program to add numbers until the user enters zero- do while loop .c
@@ -0,0 +1,18 @@
+#include  <stdio.h>
+
+int main()
+{
+	double n, sum = 0;
+  
+	do
+	{
+		printf("Enter a number : ");
+		scanf("%lf",&n);
+		sum+=n;
+	}	
+	while (n!=0.0);
+	
+	printf("Sum = %.2lf", sum);
+  
+	return 0;
+}
